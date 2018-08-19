2414ccfed563c8625376ed8651aee9847f2fa780
Eric Davis <edavis@eresources.com>
2018-08-19 14:53:58 -0700

    Split formats.go into separate format files

--- utils.go
+++ utils.go
@@ -2,6 +2,15 @@ package main
 
 import "time"
 
+const (
+	NSDublinCore = "http://purl.org/dc/elements/1.1/"
+	NSAtom       = "http://www.w3.org/2005/Atom"
+)
+
+type CDATA struct {
+	Value string `xml:",cdata"`
+}
+
 func Timestamp(fmt string, input time.Time) string {
 	switch fmt {
 	case "rss":
@@ -11,4 +20,8 @@ func Timestamp(fmt string, input time.Time) string {
 	default:
 		return input.Format(time.RFC1123Z)
 	}
+}
+
+func UTCNow() time.Time {
+	return time.Now().UTC()
 }
